fe63eafd4a5cc21077542f200e8ed2ef50e11fe2
mumian1014 <mumian1014@gmail.com>
2025-04-08 21:35:21 +0900

    Initial commit

---
+++ src/main/java/com/javajo/javajo_jewels/JavajoJewelsApplication.java
@@ -0,0 +1,13 @@
+package com.javajo.javajo_jewels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavajoJewelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavajoJewelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javajo/javajo_jewels/JavajoJewelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javajo.javajo_jewels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavajoJewelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
