5811627fef62c45495705d4192b33a121f7ccf46
mii1004 <shimadam@st.inc>
2025-05-04 18:12:57 +0900

    ProductServiceを実装

---
+++ src/test/java/com/javajo/javajo_jewels/service/ProductServiceTest.java
@@ -0,0 +1,85 @@
+package com.javajo.javajo_jewels.service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import com.javajo.javajo_jewels.entity.ProductEntity;
+import com.javajo.javajo_jewels.model.Product;
+import com.javajo.javajo_jewels.repository.ProductRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.groups.Tuple.tuple;
+
+@DataJpaTest
+@Import(ProductService.class)
+class ProductServiceTest {
+
+    @Autowired
+    ProductService productService;
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @BeforeEach
+    void setUp() {
+        var product1 = ProductEntity.builder()
+                .name("きらめきリボンブレスレット")
+                .price(new BigDecimal("200.0"))
+                .description("大きなリボンがついたブレスレット。特別な日にぴったりのアクセ。")
+                .imageUrl("http://image1.jpg")
+                .build();
+        var product2 = ProductEntity.builder()
+                .name("ふわもこユニコーンポーチ")
+                .price(new BigDecimal("550.0"))
+                .description("ふわふわ手触りのユニコーン型ポーチ。小物をかわいく収納。")
+                .imageUrl("http://image2.jpg")
+                .build();
+        var product3 = ProductEntity.builder()
+                .name("スイートキャンディボールペン")
+                .price(new BigDecimal("180.0"))
+                .description("カラフルなキャンディ風デザインのボールペン。友だちに自慢しちゃおう！")
+                .imageUrl("http://image3.jpg")
+                .build();
+        productRepository.saveAll(List.of(product1, product2, product3));
+    }
+
+    @Test
+    void getAllProductsTest() {
+        var actual = productService.getAllProducts();
+
+        assertThat(actual)
+                .hasSize(3)
+                .extracting(
+                        Product::getName,
+                        Product::getDescription,
+                        Product::getPrice,
+                        Product::getImageUrl)
+                .containsExactlyInAnyOrder(
+                        tuple("きらめきリボンブレスレット", "大きなリボンがついたブレスレット。特別な日にぴったりのアクセ。", 200, "http://image1.jpg"),
+                        tuple("ふわもこユニコーンポーチ", "ふわふわ手触りのユニコーン型ポーチ。小物をかわいく収納。", 550, "http://image2.jpg"),
+                        tuple("スイートキャンディボールペン", "カラフルなキャンディ風デザインのボールペン。友だちに自慢しちゃおう！", 180, "http://image3.jpg"));
+    }
+
+    @Test
+    void getProductByIdTest() {
+        var actual = productService.getProductById(1);
+
+        assertThat(actual)
+                .extracting(
+                        Product::getName,
+                        Product::getDescription,
+                        Product::getPrice,
+                        Product::getImageUrl)
+                .containsExactly(
+                        "きらめきリボンブレスレット",
+                        "大きなリボンがついたブレスレット。特別な日にぴったりのアクセ。",
+                        200,
+                        "http://image1.jpg");
+    }
+}
