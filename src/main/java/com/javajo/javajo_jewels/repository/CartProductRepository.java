a9ece951f848965e861d19688fe693e80c229bda
Aya Ebata <ayaaaaa.0u0w@gmail.com>
2025-05-05 20:46:15 +0900

    DBのテーブル作成と初期値を追加

--- src/main/java/com/javajo/javajo_jewels/repository/CartProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.javajo.javajo_jewels.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.javajo.javajo_jewels.entity.CartProductEntity;
-
-public interface CartProductRepository extends JpaRepository<CartProductEntity, Integer> {
-}
--- src/main/java/com/javajo/javajo_jewels/repository/CartRepository.java
+++
@@ -1,8 +0,0 @@
-package com.javajo.javajo_jewels.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.javajo.javajo_jewels.entity.CartEntity;
-
-public interface CartRepository extends JpaRepository<CartEntity, Integer> {
-}
