1158c0350d48e00ba7ea14ce023b1cf930fe526e
mumian1014 <mumian1014@gmail.com>
2025-05-16 21:15:13 +0900

    指摘事項の反映
    不要な記述を削除

--- src/test/java/com/javajo/javajo_jewels/service/OrderServiceTest.java
+++ src/test/java/com/javajo/javajo_jewels/service/OrderServiceTest.java
@@ -44,7 +44,7 @@ class OrderServiceTest {
         var product2 = ProductEntity.builder()
                 .name("虹のゆめかわヘアクリップ")
                 .price(210)
-                .description("パステルカラーの虹がきらっと光る、ゆめかわいいヘアクリップ。つけるだけで毎日がときめく魔法タイムに♪\"")
+                .description("パステルカラーの虹がきらっと光る、ゆめかわいいヘアクリップ。つけるだけで毎日がときめく魔法タイムに♪")
                 .imageUrl("/images/product-2.png")
                 .build();
         var product3 = ProductEntity.builder()
